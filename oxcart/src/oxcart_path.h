d7f17ccc0f06c66518eb37becc1b568726d48bb2
schaetj <tom.j.schaefer.jr@gmail.com>
2016-12-31 16:36:53 -0800

    Migrate NVG to PATH

--- oxcart/src/oxcart_path.h
+++ oxcart/src/oxcart_path.h
@@ -28,8 +28,56 @@
 extern "C" {
 #endif
 
+#define OXCART_PATH_LINECAP_BUTT   0x00
+#define OXCART_PATH_LINECAP_ROUND  0x01
+#define OXCART_PATH_LINECAP_SQUARE 0x02
+#define OXCART_PATH_LINEJOIN_MITER 0x00
+#define OXCART_PATH_LINEJOIN_ROUND 0x01
+#define OXCART_PATH_LINEJOIN_BEVEL 0x02
+#define OXCART_PATH_WINDING_CCW    0x00
+#define OXCART_PATH_WINDING_CW     0x01
+
+typedef struct oxcart_path_t oxcart_path_t;
+
 void oxcart_path_initialize();
 void oxcart_path_terminate();
+oxcart_path_t* oxcart_path_create();
+void oxcart_path_destroy(oxcart_path_t* path);
+void oxcart_path_beginframe(oxcart_path_t* path);
+void oxcart_path_endframe(oxcart_path_t* path);
+void oxcart_path_save(oxcart_path_t* path);
+void oxcart_path_restore(oxcart_path_t* path);
+void oxcart_path_setlinecap(oxcart_path_t* path, int linecap);
+void oxcart_path_setlinejoin(oxcart_path_t* path, int linejoin);
+void oxcart_path_setmiterlimit(oxcart_path_t* path, float limit);
+void oxcart_path_setglobalalpha(oxcart_path_t* path, float alpha);
+void oxcart_path_setstrokewidth(oxcart_path_t* path, float width);
+void oxcart_path_setstrokecolor(oxcart_path_t* path, const oxcart_vec4f_t* color);
+void oxcart_path_setfillcolor(oxcart_path_t* path, const oxcart_vec4f_t* color);
+void oxcart_path_setclip(oxcart_path_t* path, float x, float y, float w, float h);
+int oxcart_path_inclip(oxcart_path_t* path, float x, float y);
+void oxcart_path_resetclip(oxcart_path_t* path);
+void oxcart_path_beginpath(oxcart_path_t* path);
+void oxcart_path_closepath(oxcart_path_t* path);
+void oxcart_path_setpathwinding(oxcart_path_t* path, int winding);
+void oxcart_path_moveto(oxcart_path_t* path, float x, float y);
+void oxcart_path_lineto(oxcart_path_t* path, float x, float y);
+void oxcart_path_bezierto(oxcart_path_t* path, float cx1, float cy1, float cx2, float cy2, float x, float y);
+void oxcart_path_quadto(oxcart_path_t* path, float cx, float cy, float x, float y);
+void oxcart_path_arcto(oxcart_path_t* path, float x1, float y1, float x2, float y2, float radius);
+void oxcart_path_arc(oxcart_path_t* path, float cx, float cy, float r, float a0, float a1, int dir);
+void oxcart_path_rect(oxcart_path_t* path, float x, float y, float w, float h);
+void oxcart_path_roundedrect(oxcart_path_t* path, float x, float y, float w, float h, float r);
+void oxcart_path_ellipse(oxcart_path_t* path, float cx, float cy, float rx, float ry);
+void oxcart_path_circle(oxcart_path_t* path, float cx, float cy, float r);
+void oxcart_path_fill(oxcart_path_t* path);
+void oxcart_path_stroke(oxcart_path_t* path);
+void oxcart_path_identity(oxcart_path_t* path);
+void oxcart_path_rotate(oxcart_path_t* path, float angle);
+void oxcart_path_translate(oxcart_path_t* path, float x, float y);
+void oxcart_path_scale(oxcart_path_t* path, float x, float y);
+void oxcart_path_transform(oxcart_path_t* path, const oxcart_mat3f_t* m1);
+void oxcart_path_setmatrix(oxcart_path_t* path, const oxcart_mat3f_t* m1);
 
 #ifdef __cplusplus
 }
